454270fc625f49622a90001625c26493e8819d4a
patson120 <patrickkennenl@gmail.com>
2023-12-21 04:04:10 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
   content: [
     "./App.{js,jsx,ts,tsx}",
     "./app/screens/**/*.{js,jsx,ts,tsx}",
-    "./app/components/**/*.{js,jsx,ts,tsx}"
+    "./app/components/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
